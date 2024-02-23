d4e7ee8e149bdef698ff84afaff398ca008d9459
jimezarate27 <jzarate30138@ufide.ac.cr>
2024-02-22 22:13:40 -0600

    clase 5 y 6

---
+++ Tienda_WebJ/src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categoria")
+
+public class Categoria implements Serializable { //serializacion porque se va almacenar ciertos datos en el disco
+
+    private static final long serialVersionUID = 1L; //para poder hacer el ciclo de la sumatoria de la categoria.
+
+    @Id //id categoria es la llave de la tabla categoria. 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Los valores generados que estrategia usan, identico a la BD 
+    @Column(name = "id_categoria") //decir cual es el nombre en la base de datos. Se hace la asociación 
+    private long idCategoria;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+}
